1ce91b24396a5d85d7c4d65faee2e3cecfe2ea93
Strahinja Jankovic <strahinja.p.jankovic@gmail.com>
2025-09-06 20:31:42 +0200

    i2c-driver: Add device tree overlay support
    Signed-off-by: Strahinja Jankovic <strahinja.p.jankovic@gmail.com>

--- recipes-bsp/u-boot/files/cubieboard-ng/boot.cmd
+++ recipes-bsp/u-boot/files/cubieboard-ng/boot.cmd
@@ -13,6 +13,15 @@ if test -z "$altbootcmd"; then
 fi
 
 setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p${rootpart} rootwait panic=10 ${extra}
+
+# Load device tree and overlay
+setenv i2c_overlay cubieboard-ng-i2csens-overlay.dtbo
 load mmc 0:${rootpart} ${fdt_addr_r} boot/${fdtfile}
+load mmc 0:${rootpart} ${fdtoverlay_addr_r} boot/${i2c_overlay}
+fdt addr ${fdt_addr_r}
+fdt resize
+fdt apply ${fdtoverlay_addr_r}
+
+# Load kernel and boot
 load mmc 0:${rootpart} ${kernel_addr_r} boot/uImage
 bootm ${kernel_addr_r} - ${fdt_addr_r}
